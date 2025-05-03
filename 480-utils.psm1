b7e02cc610c24eda3ba5f9e524c5cf89c565edd0
AinsleyPlayer <90421152+AinsleyPlayer@users.noreply.github.com>
2025-05-03 06:06:20 -0400

    Update 480-utils.psm1

--- 480-utils.psm1
+++ 480-utils.psm1
@@ -3,14 +3,6 @@ function 480Banner()
     Write-Host "Hello SYS480-Devops"
 }
 
-
-(second rendition)
-
-function 480Banner()
-{
-    Write-Host "Hello SYS480-Devops"
-}
-
 function 480Connect([string] $server)
 {
     $conn = $global:DefaultVIServer
@@ -20,8 +12,46 @@ function 480Connect([string] $server)
 
       Write-Host -ForeGroundColor Green $msg
 
-    }else
+    }
+    else{
+        $conn=Connect-VIServer -Server $server
+    }
+}
+
+function Get-480Config([string] $config_path){
+    Write-Host "Reading" $sconfig_path
+    $conf=$null
+    if(Test-Path $config_path){
+        $conf = (Get-Content -Raw -Path $config_path | ConvertFrom-Json)
+    }
+    else{
+        Write-Host -ForegroundColor "yellow" "No configuration found"
+    }
+    return $conf
+}
+
+function Select-VM([string] $folder)
+{
+
+    $selected_vm = $null
+    try
     {
-        $conn = Connect-VIServer -Server $server
+
+        $vms = Get-VM -Location $folder
+        $index = 1
+        foreach($vm in $vms)
+        {
+            Write-Host [$index] $vm.name
+            $index = 1
+        }
+        $pick_index = Read-Host "Which Index Number [x] do you wish to pick?"
+        $selected_vm = $vms[$pick_index -1]
+        Write-Host "You Picked  " $selected_vm.name
+        return $selected_vm
+    }
+    catch
+    {
+        Write-Host "Invalid Folder: $folder" -ForegroundColor "Red"
+
     }
 }
