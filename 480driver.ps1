83931fe5638222bd7a3c5f48adbdc14c2c2b5171
AinsleyPlayer <90421152+AinsleyPlayer@users.noreply.github.com>
2025-05-03 09:02:46 -0400

    Update 480driver.ps1

--- 480driver.ps1
+++ 480driver.ps1
@@ -1,4 +1,5 @@
 Import-Module '480-utils' -Force
 480Banner
-#$conf = Get-480Config -config_path = /home/ainsley/modules/480.json"
-cloner -config_path ./480.json
+$conf = Get-480Config -config_path = /home/ainsley/modules/480.json"
+480Connect -server $conf.vcenter_server
+#cloner -config_path ./480.json
