ad7673e54035046b40351fabec379bff678690d0
AinsleyPlayer <90421152+AinsleyPlayer@users.noreply.github.com>
2025-05-03 04:31:22 -0400

    Update 480-utils.psm1

--- 480-utils.psm1
+++ 480-utils.psm1
@@ -2,3 +2,26 @@ function 480Banner()
 {
     Write-Host "Hello SYS480-Devops"
 }
+
+
+(second rendition)
+
+function 480Banner()
+{
+    Write-Host "Hello SYS480-Devops"
+}
+
+function 480Connect([string] $server)
+{
+    $conn = $global:DefaultVIServer
+    #connected?
+    if [$conn]{
+      $msg = "You are already connected to: {0}" -f $conn
+
+      Write-Host -ForeGroundColor Green $msg
+
+    }else
+    {
+        $conn = Connect-VIServer -Server $server
+    }
+}
